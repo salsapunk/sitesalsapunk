e4c2cb0b49f0de11f8be494dd7bfa00bcedd8853
salsapunk <bernardinhogamespray@gmail.com>
2025-03-21 13:21:06 -0300

    mudou tudo + js

---
+++ app.js
@@ -0,0 +1,42 @@
+function redirectMusic(e) {
+    switch (e.target.id) {
+        case '1':
+            window.location.href = 'https://youtu.be/jPgA0wtWu0I?si=nBHf-xLbg7NbXW8N';
+            break;
+        case '2':
+            window.location.href = 'https://youtu.be/71A_MB_-ozM?si=EFLIh9THNq_cjTRL';
+            break;
+        case '3':
+            window.location.href = 'https://youtu.be/6vNd1F0b8rg?si=zwMe28ALcvB6jm4_';
+            break;
+        case '4':
+            window.location.href = 'https://youtu.be/FXPFGyAUiWQ?si=wFm5mawso-Jp7BqN';
+            break;
+        case '5':
+            window.location.href = 'https://youtu.be/nCP-ivnALrE?si=OZtmO8K05IYQAZ8z';
+            break;
+    }
+}
+
+function redirectSocial(e) {
+    switch (e.target.id) {
+        case '1':
+            window.location.href = '';
+            break;
+        case '2':
+            window.location.href = 'https://www.instagram.com/bernardojamesss?igsh=NW5jdnV4emNpOHhv';
+            break;
+        case '3':
+            window.location.href = 'https://www.instagram.com/p0t0ko/';
+            break;
+        case '4':
+            window.location.href = 'https://www.instagram.com/kaka.img/';
+            break;
+        case '5':
+            window.location.href = 'https://www.instagram.com/christiangabrielalbert/';
+            break;
+        case '6':
+            window.location.href = '';
+            break;
+    }
+}
